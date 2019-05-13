d7b78c40ed3228ede8dc3ad71acf7ab512803be1
petrtopor <coder.ru@gmail.com>
2019-05-14 02:30:33 +0400

    try to intercept the 'message_new' event

--- api/callbackvk.js
+++ api/callbackvk.js
@@ -4,12 +4,28 @@ module.exports = function (server, mongoose, logger) {
     path: '/callbackvk',
     config: {
       handler: (request, h) => {
+        const type = request.payload['type']
+        switch (type) {
+          case 'confirmation': {
+            return '1d637612'
+          }
+          case 'message_new': {
+            const object = request.payload['object']
+            console.log('message_new:\n', object['text'])
+            return 'ok'
+          }
+          default: {
+            return 'ok'
+          }
+        }
+        /*
         if(request.payload['type'] === 'confirmation' && (request.payload['group_id'] === '182152948' || request.payload['group_id'] === 182152948)) {
           return '1d637612'
         } else {
           // return request.payload
           return 0
         }
+        */
       }
     }
   })
